0d2a3b336acd6eeef84c193323650b507fe9112e
Preetam Mukherjee <21f1002436@ds.study.iitm.ac.in>
2023-09-30 22:37:46 +0530

    Save

--- app.py
+++ app.py
@@ -1,1 +1,56 @@
-print('Hello World')
+import os 
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+#from src.models.models import db
+#from src.controllers import login_controller
+
+app = Flask(__name__)
+app.config('SQLALCHEMY_DATABASE_URI') = 'sqlite.///grocery_store.db'
+db = SQLAlchemy(app)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    products = db.relationship('Product', backref = 'category', lazy = True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    manufacture_date = db.Column(db.String(20))
+    expiry_date= db.Column(db.String(20))
+    rate_per_unit = db.Column(db.Float)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable = False)
+
+@app.route('/')
+def index():
+    categories = Category.query.all()
+    return render_template('index.html', categories = categories)
+
+@app.route('/add_category', methods = ['POST'])
+def add_category():
+    if request.method == 'POST':
+        name = request.form['name']
+        category = Category(name = name)
+        db.session.add(category)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/add_product', methods = ['POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        manufacture_date = request.form['manufacture_date']
+        expiry_date = request.form['expiry_date']
+        rate_per_unit = float(request.form['rate_per_unit'])
+        category_id = int(request.form['category_id'])
+        product = Product(name = name, manufacture_date = manufacture_date, expiry_date = expiry_date, 
+                          rate_per_unit = rate_per_unit, category_id = category_id)
+        db.session.add(product)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug = True)
+    
+
